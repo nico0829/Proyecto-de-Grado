8a7052e95f68e62f8ca3f3e6d868d6f20bc98f2f
Nicolas <nico0829@utp.edu.co>
2019-01-25 12:20:51 -0500

    URL PRUEBA ALFA

--- src/pages/serhumanentesocial/serhumanentesocial.ts
+++ src/pages/serhumanentesocial/serhumanentesocial.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+
 /**
  * Generated class for the SerhumanentesocialPage page.
  *
@@ -21,5 +22,6 @@ export class SerhumanentesocialPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad SerhumanentesocialPage');
   }
+
 
 }
